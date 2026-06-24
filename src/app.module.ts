ca275075b7b87be589e1156ad57f0f604343a583
Serwach <mateusz.serwach.1@gmail.com>
2026-06-24 11:02:14 +0200

    adding entities to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { User } from './users/user.entity';
+import { Product } from './products/product.entity';
+import { Order } from './orders/order.entity';
+import { OrderItem } from './orders/order-item.entity';
 
 @Module({
   imports: [
@@ -12,9 +19,12 @@ import { AppService } from './app.service';
       username: 'root',
       password: 'root',
       database: 'ecommerce',
-      entities: [],
+      entities: [User, Product, Order, OrderItem],
       synchronize: true,
     }),
+    UsersModule,
+    ProductsModule,
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
